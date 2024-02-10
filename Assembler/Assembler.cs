55aaa7fbd442b51c4b18c6048281b957b4bb04e6
larunnel161 <larunnel161@gmail.com>
2024-02-09 19:35:56 -0600

    Defining the rest of the instructions for now. Mostly inspired by MIPS ISA

--- Assembler/Assembler.cs
+++ Assembler/Assembler.cs
@@ -9,6 +9,8 @@ namespace Compiler.Assembler;
 public class Assembler {
     public static void assembleFile(string filePath) {
         TokenType test = TokenType.add_Inst;
+        byte byteTest = 100;
+        byte byteTest2 = (byte) ((byteTest) << 1);
         Console.WriteLine(filePath);
     }
 

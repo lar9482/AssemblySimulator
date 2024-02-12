cf046c2fbcdaf61a40dc0868e10a3a239f5a75ae
larunnel161 <larunnel161@gmail.com>
2024-02-11 18:34:44 -0600

    Making sure the jmp instructions have the proper inheritance

--- Assembler/Instruction/JmpLabelInst.cs
+++ Assembler/Instruction/JmpLabelInst.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Compiler.Assembler.Instruction.AssembledInst;
+
 namespace Compiler.Assembler.Instruction;
 
-public class JmpLabelInst {
+public class JmpLabelInst : Inst {
     private string label;
 
-    public JmpLabelInst(string label) {
+    public JmpLabelInst(string label, string instName, InstType type)
+    : base(instName, type) {
         this.label = label;
+    }
+
+    public override AsmInst assembleInst() {
+        throw new NotImplementedException();
     }
 }
--- Assembler/Instruction/JmpRegInst.cs
+++ Assembler/Instruction/JmpRegInst.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Compiler.Assembler.Instruction.AssembledInst;
+
 namespace Compiler.Assembler.Instruction;
 
-public class JmpRegInst {
+public class JmpRegInst : Inst {
     private string reg;
 
-    public JmpRegInst(string reg) {
+    public JmpRegInst(string reg, string instName, InstType type)
+    : base(instName, type) {
         this.reg = reg;
+    }
+
+    public override AsmInst assembleInst() {
+        throw new NotImplementedException();
     }
 }

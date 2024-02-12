bfa2daa632e1a487263db3b372633a8c8c24ffe0
lar9482 <lar9482@utulsa.edu>
2024-02-11 18:34:44 -0600

    Making sure the jmp instructions have the proper inheritance

--- Assembler/Instruction/JmpBranchInst.cs
+++ Assembler/Instruction/JmpBranchInst.cs
@@ -3,17 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Compiler.Assembler.Instruction.AssembledInst;
+
 namespace Compiler.Assembler.Instruction;
 
-public class JmpBranchInst {
+public class JmpBranchInst : Inst {
     private string reg1;
     private string reg2;
 
     private string label;
 
-    public JmpBranchInst(string reg1, string reg2, string label) {
+    public JmpBranchInst(string reg1, string reg2, string label, string instName, InstType type) 
+    : base(instName, type) {
         this.reg1 = reg1;
         this.reg2 = reg2;
         this.label = label;
+    }
+
+    public override AsmInst assembleInst() {
+        throw new NotImplementedException();
     }
 }

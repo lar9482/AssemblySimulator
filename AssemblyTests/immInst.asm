5de859544b70b85404f440dcb86c27911d95a51e
lar9482 <lar9482@utulsa.edu>
2024-02-16 17:09:47 -0600

    Decoding immediate format from the instructions

--- AssemblyTests/immInst.asm
+++ AssemblyTests/immInst.asm
@@ -4,8 +4,8 @@ movI r9, -1000
 addI r1, 100
 subI r2, 100
 multI r3, 10
-divI r4, 10
-orI r5, 10
-xorI r6, 10
-sll r14, 10
-sra r16, 10
+divI r4, 11
+orI r5, 12
+xorI r6, 13
+sll r14, 14
+sra r16, 54354
